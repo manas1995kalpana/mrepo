c5f95623eddc49d13bf31cf0e53f7310eb6268a3
Manas Ranjan Rana <mrrana876@gmail.com>
2025-08-07 17:18:42 +0530

    first commit

---
+++ src/main/java/com/ebx/multiserv/MultiservApplication.java
@@ -0,0 +1,13 @@
+package com.ebx.multiserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebx/multiserv/MultiservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebx.multiserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
