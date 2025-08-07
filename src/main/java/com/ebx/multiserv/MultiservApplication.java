4819848d2c22efbfc06e950aa3ad6130bfe2bfdb
Manas Ranjan Rana <mrrana876@gmail.com>
2025-08-08 00:41:00 +0530

    new1

--- src/main/java/com/ebx/multiserv/MultiservApplication.java
+++ src/main/java/com/ebx/multiserv/MultiservApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MultiservApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args1) {
 		SpringApplication.run(MultiservApplication.class, args);
 	}
 
